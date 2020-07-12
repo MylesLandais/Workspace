67f5b0c8b075ef9ac08fda0797c73b2b2d65733e
myles <landais.myles@gmail.com>
2020-07-12 14:09:15 -0500

    pass diffsquares

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,18 +2,20 @@ package diffsquares
 
 func SquareOfSum(n int) int{
 	sum := 0
-	i := 0
+	i := 1
 	for i <= n{
 		sum += i
+		i++
 	}
 	return sum * sum
 }
 
 func SumOfSquares(n int) int{
 	sum := 0
-	i := 0
+	i := 1
 	for i <= n{
 		sum += i*i
+		i++
 	}
 	return sum
 }
