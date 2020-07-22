ecf6ac8ccb13d6de6d4517ef35cf4a1ab96e2535
myles <landais.myles@gmail.com>
2020-07-22 11:57:59 -0500

    complete grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,26 +1,41 @@
 package grains
 
-func powtwo(n uint64) uint64 {
-	var r uint64 = 1
-	var i uint64
-	if n != 0 {
-		for i = 1; i <= n; i++ {
-			r = r * 2
-		}
+import (
+	"errors"
+)
+
+// func powtwo(n uint64) uint64 {
+// 	var r uint64 = 1
+// 	var i uint64
+// 	if n != 0 {
+// 		for i = 1; i <= n; i++ {
+// 			r = r * 2
+// 		}
+// 	}
+// 	return r
+// }
+
+//Square this
+func Square(n int) (uint64, error) {
+	var total uint64 = 1
+	if n == 1 {
+		return total, nil
 	}
-	return r
+	if n > 1 && n <= 64 {
+		for i := 0; i < n-1; i++ {
+			total *= 2
+		}
+		return total, nil
+	}
+	return total, errors.New("Some error")
 }
 
-func Square(n uint64) (uint64, error) {
-	i := powtwo(n)
-	return i, nil
-}
-
+//Total that
 func Total() uint64 {
 	var sum uint64 = 0
-	var i uint64
-	for i = 0; i <= 63; i++ {
-		n, _ := uint64(Square(i))
+	var i int
+	for i = 1; i <= 64; i++ {
+		n, _ := Square(i)
 		sum += n
 	}
 	return sum
