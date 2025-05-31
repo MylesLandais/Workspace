57591d445fef99ba78d649b9ddb01d9132826f90
Jovan <61637503+jov4n@users.noreply.github.com>
2025-05-30 18:41:59 -0600

    Update runtime.exs
    fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,14 +2,17 @@
 import Config
 import Dotenvy
 
-source!(Path.expand(".env"))
+if File.exists?(".env") do
+  source!(Path.expand(".env"))
+end
 
 config :nostrum,
   token: env!("DISCORD_TOKEN", :string!),
   gateway_intents: [:guilds, :guild_messages, :guild_voice_states, :message_content],
   youtubedl: "yt-dlp",
-  ffmpeg: "ffmpeg"
-  
+  ffmpeg: "ffmpeg",
+  voice_log_level: :debug
 
 config :nebulixir,
   discord_guild_id: env!("GUILD_ID", :string!)
+
